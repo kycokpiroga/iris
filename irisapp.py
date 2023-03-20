5dfbca4a80c62cc06c0d153742a5e69490025d53
kycokpiroga <38364329+kycokpiroga@users.noreply.github.com>
2023-03-20 21:04:39 +0500

    Update irisapp.py

--- irisapp.py
+++ irisapp.py
@@ -1,5 +1,6 @@
 import pandas as pd
 import streamlit as st
+import pycaret
 from pycaret.regression import load_model, predict_model
 st.set_page_config(page_title="Iris Classification App")
 @st.cache_data()
